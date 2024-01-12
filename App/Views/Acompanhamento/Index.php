42c9ad88adc811e97361a424d334d99e718b26c8
lucas sanches <lucassanches12398@hotmail.com>
2024-01-11 22:18:49 -0300

    adicionado a flag da frança no card

--- App/Views/Acompanhamento/Index.php
+++ App/Views/Acompanhamento/Index.php
@@ -5,54 +5,58 @@
 
 <?php foreach ($this->getViewVar()['acompanhamento'] as $index => $acompanhamento) { ?>
     <div class="accordion">
-        <div class="card mb-3 custom-card">
-            <div class="row g-0">
-                <div class="col-md-2 text-center">
-                    <div class="card-body text-center custom-card-body"> 
-                        <?php if($acompanhamento['colocacao'] <= 20){?>
+        <div class="card mb-3 custom-card diagonal-background">
+            <div class="diagonal-background-white">
+                <div class="diagonal-background-red">
+                    <div class="row g-0">
+                        <div class="col-md-2 text-center">
+                            <div class="card-body text-center custom-card-body "> 
+                                <?php if($acompanhamento['colocacao'] <= 20){?>
+                                    <div class="d-flex justify-content-center align-items-center h-60">
+                                        <img src="<?= PATH_USERS_IMG . 'App/Views/Imagens/Medalhas/'. $acompanhamento['colocacao'] .'.png'?>" alt="" width="300" height="140" class="imagem-colocacao"> 
+                                    </div>
+                                <?php }else{ ?>
+                                    <div class="card-body text-center custom-card-body"> 
+                                        <p class="colocacao-format"><?= $acompanhamento['colocacao']?>ª</p> 
+                                    </div>
+                                <?php } ?>
+                            </div>
+                        </div>
+                        <div class="col-md-3 text-center">
                             <div class="d-flex justify-content-center align-items-center h-100">
-                                <img src="<?= PATH_USERS_IMG . 'App/Views/Imagens/Medalhas/'. $acompanhamento['colocacao'] .'.png'?>" alt="" width="300" height="140" class="imagem-colocacao"> 
+                                <img src="<?= PATH_USERS_IMG . $acompanhamento['caminho_imagem'] ?>" alt="Imagem" width="180" height="180" class="imagem-destaque"> 
                             </div>
-                        <?php }else{ ?>
+                        </div>
+                        <div class="col-md-2">
                             <div class="card-body text-center custom-card-body"> 
-                                <p class="colocacao-format"><?= $acompanhamento['colocacao']?>ª</p> 
+                                <h5 class="custom-card-body-title" data-toggle="collapse" data-target="#collapse<?= $index ?>" aria-expanded="false" aria-controls="collapse<?= $index ?>">Loja</h5>
+                                <p class="custom-card-body-text"><?= $acompanhamento['filial']?></p>
                             </div>
-                        <?php } ?>
-                    </div>
-                </div>
-                <div class="col-md-3 text-center">
-                    <div class="d-flex justify-content-center align-items-center h-100">
-                        <img src="<?= PATH_USERS_IMG . $acompanhamento['caminho_imagem'] ?>" alt="Imagem" width="180" height="180" class="imagem-destaque"> 
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="card-body text-center custom-card-body"> 
-                        <h5 class="custom-card-body-title" data-toggle="collapse" data-target="#collapse<?= $index ?>" aria-expanded="false" aria-controls="collapse<?= $index ?>">Loja</h5>
-                        <p class="custom-card-body-text"><?= $acompanhamento['filial']?></p>
-                    </div>
-                </div>
-                <div class="col-md-1">
-                    <div class="card-body text-center custom-card-body"> 
-                        <h5 class="custom-card-body-title">Ouro</h5>
-                        <p class="custom-card-body-text"><?= $acompanhamento['ouro']?></p>
-                    </div>
-                </div>
-                <div class="col-md-1">
-                    <div class="card-body text-center custom-card-body"> 
-                        <h5 class="custom-card-body-title">Prata</h5>
-                        <p class="custom-card-body-text"><?= $acompanhamento['prata']?></p>
-                    </div>
-                </div>
-                <div class="col-md-1">
-                    <div class="card-body text-center custom-card-body"> 
-                        <h5 class="custom-card-body-title">Bronze</h5>
-                        <p class="custom-card-body-text"><?= $acompanhamento['bronze']?></p>
-                    </div>
-                </div>
-                <div class="col-md-2">
-                    <div class="card-body text-center custom-card-body"> 
-                        <h5 class="custom-card-body-title">Score</h5>
-                        <p class="custom-card-body-text"><?= $acompanhamento['score']?></p>
+                        </div>
+                        <div class="col-md-1">
+                            <div class="card-body text-center custom-card-body"> 
+                                <h5 class="custom-card-body-title">Ouro</h5>
+                                <p class="custom-card-body-text"><?= $acompanhamento['ouro']?></p>
+                            </div>
+                        </div>
+                        <div class="col-md-1">
+                            <div class="card-body text-center custom-card-body"> 
+                                <h5 class="custom-card-body-title">Prata</h5>
+                                <p class="custom-card-body-text"><?= $acompanhamento['prata']?></p>
+                            </div>
+                        </div>
+                        <div class="col-md-1">
+                            <div class="card-body text-center custom-card-body"> 
+                                <h5 class="custom-card-body-title">Bronze</h5>
+                                <p class="custom-card-body-text"><?= $acompanhamento['bronze']?></p>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="card-body text-center custom-card-body"> 
+                                <h5 class="custom-card-body-title">Score</h5>
+                                <p class="custom-card-body-text"><?= $acompanhamento['score']?></p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
